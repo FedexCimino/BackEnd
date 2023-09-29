6f5c7c5980f82be4512e1a868e4928a6838a82f4
FedexCimino <146402215+FedexCimino@users.noreply.github.com>
2023-09-29 07:29:24 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,10 +1,11 @@
-import os
+# Configuración de la base de datos
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'tu_usuario',
+    'password': 'tu_contraseña',
+    'database': 'nombre_base_de_datos'
+}
 
-# Configuración de la base de datos (MySQL)
-SQLALCHEMY_DATABASE_URI = 'mysql://usuario:contraseña@localhost/nombre_base_de_datos'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# Configuración secreta para proteger la sesión de usuario (debes generar una clave secreta)
+# Configuración de la aplicación Flask
 SECRET_KEY = 'tu_clave_secreta'
-
-UPLOAD_FOLDER = 'ruta_de_tu_directorio_de_imagenes'
+DEBUG = True
